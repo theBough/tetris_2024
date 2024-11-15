bdce2c152ab9cf3d944ac9702d3ee9cd3420accf
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-11-15 13:27:08 -0600

    Create block.js

---
+++ block.js
@@ -0,0 +1,12 @@
+function Block(x,y,w,h,col){
+  this.x = x;
+  this.y = y;
+  this.w = w;
+  this.h = h;
+  this.col = col;
+  
+  this.display = function(){
+    rect(this.x, this.y, this.w, this.h)
+  }
+  
+}
