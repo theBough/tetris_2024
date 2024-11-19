bd444dfeeba5638dec17b0bfd1e005bc4d7e5c18
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-11-19 12:48:58 -0600

    Update grid.js

--- grid.js
+++ grid.js
@@ -2,7 +2,7 @@ grid = [];
 function makeGrid(thisWidth, thisHeight) {
   for (let j = 0; j < 10; j++) {
     for (let i = 0; i < 20; i++) {
-      grid.push(new Block(i * 20, j*20, 20, 20, 120));
+      grid.push(new Block(i * 30, j*30, 30, 30, 100));
     }
   }
 }
