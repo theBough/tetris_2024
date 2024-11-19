bfaca2f4a3b7042a379d19cb9a5bbebf561cded3
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-11-19 13:05:52 -0600

    Update crossTetromino.js

--- tetromino/crossTetromino.js
+++ tetromino/crossTetromino.js
@@ -1,9 +1,8 @@
-
 function makeCross(){
+  grid[61].col = "pink"
+  grid[80].col = "pink"
   grid[81].col = "pink"
   grid[82].col = "pink"
-  grid[83].col = "pink"
-  grid[72].col = "pink"
-  grid[92].col = "pink"
-  grid[102].col = "pink"
+  grid[101].col = "pink"
+  grid[121].col = "pink"
 }
