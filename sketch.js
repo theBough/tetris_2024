633322443340ff226e1be36594c27ea60c0df9b8
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-11-25 12:56:54 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,24 @@
-activeTetromino = []
+let activeTetromino = []
+let myGravity;
+let hitEdge = false;
 function setup() {
   createCanvas(600, 300);
   makeGrid()
   makeCross()
+  gravityOn()
 }
 function draw() {
   background(220);
   for(let i=0 ; i<grid.length; i++){
     grid[i].display()
+    
   }
+  
+  if(hitEdge){
+    clearInterval(myGravity);
+  }
+}
+function gravityOn(){
+  //console.log("test")
+  myGravity = setInterval(moveActiveTetromino,500)
 }
