094b02915ab1d1cb6efde9d39130907a95f1756f
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-11-26 12:58:15 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,24 @@
+let colourList = []
+let activeColour
 let activeTetromino = []
 let myGravity;
 let hitEdge = false;
 function setup() {
   createCanvas(600, 300);
   makeGrid()
-  makeCross()
   gravityOn()
+  colourList.push("pink")
+  colourList.push("rgb(192,255,255)")
+  
+  let index = Math.trunc(random(0,colourList.length))
+  console.log(index)
+  activeColour = colourList[index]
+  console.log(activeColour)
+  if(index == 0){
+    makeCross();
+  } else if(index == 1){
+    makeTophat()
+  }
 }
 function draw() {
   background(220);
