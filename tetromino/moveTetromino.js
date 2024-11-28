079abdfdd502d14a95272eeb28084e6e1718299b
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-11-28 12:47:51 -0600

    Update moveTetromino.js

--- tetromino/moveTetromino.js
+++ tetromino/moveTetromino.js
@@ -1,5 +1,5 @@
 function moveActiveTetromino(){
-
+  
   for(let i = 0; i<activeTetromino.length;i++){
     activeTetromino[i] +=1
     if(activeTetromino[i] % 20 == 19){
@@ -10,7 +10,23 @@ function moveActiveTetromino(){
   }
   
     for(let i = 0; i<activeTetromino.length;i++){
-      grid[activeTetromino[i]].col = activeColour;
-    }   
+      grid[activeTetromino[i]].col = activeColour
+    }    
 
+}
+function keyReleased(){
+  if(key === 'w'){
+    //move the tetromino up
+   for(let i = 0; i<activeTetromino.length;i++){
+     activeTetromino[i] -= 20
+    }
+    //true the old spot grey
+    for(let i = 0; i<activeTetromino.length;i++){
+      grid[activeTetromino[i]+20].col = 100
+    } 
+    //colour new location of tetromino
+    for(let i = 0; i<activeTetromino.length;i++){
+      grid[activeTetromino[i]].col = activeColour
+    } 
+  }
 }
