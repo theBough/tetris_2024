51f888eddda91258adebefaa0011a3b06dca19f0
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-12-04 13:25:03 -0600

    Update crossHexamino.js

--- hexamino/crossHexamino.js
+++ hexamino/crossHexamino.js
@@ -1,6 +1,6 @@
 function makeCross(){
-  activeHexamino = [61,80,81,82,101,121,0];
-  for(let i = 0 ; i<activeHexamino.length-1; i++){
-    grid[activeHexamino[i]].col = "pink"
+  activeHexomino = [61,80,81,82,101,121,0];
+  for(let i = 0 ; i<activeHexomino.length-1; i++){
+    grid[activeHexomino[i]].col = "#DA2C38"
   }
 }
