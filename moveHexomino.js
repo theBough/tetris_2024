0c8d3181f871f43fec3fd7a74faed39ccc302e5c
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-12-04 13:24:23 -0600

    Update moveHexomino.js

--- moveHexomino.js
+++ moveHexomino.js
@@ -1,89 +1,89 @@
-function moveActiveHexamino(){
+function moveActiveHexomino(){
   
-  for(let i = 0; i<activeHexamino.length-1;i++){
-    activeHexamino[i] +=1
-    if(activeHexamino[i] % 20 == 19){
+  for(let i = 0; i<activeHexomino.length-1;i++){
+    activeHexomino[i] +=1
+    if(activeHexomino[i] % 20 == 19){
       hitEdge = true
     }
-    grid[activeHexamino[i]-1].col = 100
+    grid[activeHexomino[i]-1].col = 100
 
   }
   
-    for(let i = 0; i<activeHexamino.length-1;i++){
-      grid[activeHexamino[i]].col = activeColour
+    for(let i = 0; i<activeHexomino.length-1;i++){
+      grid[activeHexomino[i]].col = activeColour
     }    
 
 }
 function keyReleased(){
   if(key === 'w'){
-    for(let i = 0; i<activeHexamino.length-1;i++){
-     if(activeHexamino[i] < 20){
+    for(let i = 0; i<activeHexomino.length-1;i++){
+     if(activeHexomino[i] < 20){
        //we are on the edge, and cant move up
        return 0;
      }
     }
     //move the tetromino up
-   for(let i = 0; i<activeHexamino.length-1;i++){
-     activeHexamino[i] -= 20
+   for(let i = 0; i<activeHexomino.length-1;i++){
+     activeHexomino[i] -= 20
     }
     //true the old spot grey
-    for(let i = 0; i<activeHexamino.length-1;i++){
-      grid[activeHexamino[i]+20].col = 100
+    for(let i = 0; i<activeHexomino.length-1;i++){
+      grid[activeHexomino[i]+20].col = 100
     } 
     //colour new location of tetromino
-    for(let i = 0; i<activeHexamino.length-1;i++){
-      grid[activeHexamino[i]].col = activeColour
+    for(let i = 0; i<activeHexomino.length-1;i++){
+      grid[activeHexomino[i]].col = activeColour
     } 
   }
   if(key === 's'){
-    for(let i = 0; i<activeHexamino.length-1;i++){
-     if(activeHexamino[i] > 180){
+    for(let i = 0; i<activeHexomino.length-1;i++){
+     if(activeHexomino[i] > 180){
        //we are on the edge, and cant move down
        return 0;
      }
     }
     //move the tetromino down
-   for(let i = 0; i<activeHexamino.length-1;i++){
-     activeHexamino[i] += 20
+   for(let i = 0; i<activeHexomino.length-1;i++){
+     activeHexomino[i] += 20
     }
     //true the old spot grey
-    for(let i = 0; i<activeHexamino.length-1;i++){
-      grid[activeHexamino[i]-20].col = 100
+    for(let i = 0; i<activeHexomino.length-1;i++){
+      grid[activeHexomino[i]-20].col = 100
     }
     //colour new location of tetromino
-    for(let i = 0; i<activeHexamino.length-1;i++){
-      grid[activeHexamino[i]].col = activeColour
+    for(let i = 0; i<activeHexomino.length-1;i++){
+      grid[activeHexomino[i]].col = activeColour
     }
   }
   if(key === 'a'){
     if(activeColour =="#226F54"){
       //the Tophat
 
-      activeHexamino[2] -= 39
-      activeHexamino[5] += 18
+      activeHexomino[2] -= 39
+      activeHexomino[5] += 18
 
       //true the old spot grey
-      grid[activeHexamino[2]+39].col = 100
-      grid[activeHexamino[5]-18].col = 100
+      grid[activeHexomino[2]+39].col = 100
+      grid[activeHexomino[5]-18].col = 100
       //colour new location of tetromino
-      for(let i = 0; i<activeHexamino.length-1;i++){
-        grid[activeHexamino[i]].col = activeColour
+      for(let i = 0; i<activeHexomino.length-1;i++){
+        grid[activeHexomino[i]].col = activeColour
       } 
     } else if(activeColour == "#DA2C38"){
-      activeHexamino[5] -= 42
+      activeHexomino[5] -= 42
 
       //true the old spot grey
-      grid[activeHexamino[5]+42].col = 100
+      grid[activeHexomino[5]+42].col = 100
       //colour new location of tetromino
-      for(let i = 0; i<activeHexamino.length-1;i++){
-        grid[activeHexamino[i]].col = activeColour
+      for(let i = 0; i<activeHexomino.length-1;i++){
+        grid[activeHexomino[i]].col = activeColour
       } 
     }
 
   }
    if(key === 'q'){
      
-      whatHexamino();
+      whatHexomino();
      /*
       if(activeColour == "pink"){
         activeTetromino[5] -= 38
@@ -98,7 +98,7 @@ function keyReleased(){
   }
 }
 
-function whatHexamino(){
+function whatHexomino(){
   
   switch(nameOfActiveHexo){
       case "topHat":
