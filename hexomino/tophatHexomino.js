9f3efdf017b54af7ab50e98bb208698a60ce9681
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-12-04 13:58:23 -0600

    Update tophatHexomino.js

--- hexomino/tophatHexomino.js
+++ hexomino/tophatHexomino.js
@@ -20,18 +20,24 @@ function topHatState(){
       moveOneToTwo()
       activeHexomino[6] +=1
     break;  
-    case "2":
+    case 2:
+      moveTwoToThree();
       activeHexomino[6] +=1
     break;
-    case "3":
+    case 3:
+      moveThreeToZero();
       activeHexomino[6] =0
     break;  
   }
 }
 function moveZeroToOne(){
         //rotate theTopHat from zero to one
+         activeHexomino[6] += 1000
+        activeHexomino.sort(function(a,b){return a-b})
+        activeHexomino[6] -= 1000
         activeHexomino[2] += 22
         activeHexomino[5] -= 41
+  //console.log(activeHexomino)
         //turn the old spot grey
         grid[activeHexomino[2]-22].col = 100
         grid[activeHexomino[5]+41].col = 100
@@ -46,11 +52,47 @@ function moveOneToTwo(){
         activeHexomino[6] += 1000
         activeHexomino.sort(function(a,b){return a-b})
         activeHexomino[6] -= 1000
+  //console.log(activeHexomino)
         activeHexomino[0] += 18
         activeHexomino[5] -= 39
         //turn the old spot grey
         grid[activeHexomino[0]-18].col = 100
         grid[activeHexomino[5]+39].col = 100
+        //colour new location of hexomino
+        for(let i = 0; i<activeHexomino.length-1;i++){
+          grid[activeHexomino[i]].col = activeColour
+        }          
+}
+function moveTwoToThree(){
+   //rotate theTopHat from zero to one
+      
+        activeHexomino[6] += 1000
+        activeHexomino.sort(function(a,b){return a-b})
+        activeHexomino[6] -= 1000
+        
+        activeHexomino[0] -= 19
+        activeHexomino[3] += 38
+  console.log(activeHexomino)
+        //turn the old spot grey
+        grid[activeHexomino[0]+19].col = 100
+        grid[activeHexomino[3]-38].col = 100
+        //colour new location of hexomino
+        for(let i = 0; i<activeHexomino.length-1;i++){
+          grid[activeHexomino[i]].col = activeColour
+        }          
+}
+function moveThreeToZero(){
+   //rotate theTopHat from zero to one
+      
+        activeHexomino[6] += 1000
+        activeHexomino.sort(function(a,b){return a-b})
+        activeHexomino[6] -= 1000
+        
+        activeHexomino[0] += 39
+        activeHexomino[5] -= 18
+        //turn the old spot grey
+        grid[activeHexomino[0]-39].col = 100
+        grid[activeHexomino[5]+18].col = 100
         //colour new location of hexomino
         for(let i = 0; i<activeHexomino.length-1;i++){
           grid[activeHexomino[i]].col = activeColour
