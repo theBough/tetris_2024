09c78517a8dde70104a9f0610c50137176d36c7e
corbin Boughen <50217007+theBough@users.noreply.github.com>
2025-01-07 13:25:24 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,53 +1,52 @@
-let colourList = []
-let activeColour
-let activeHexomino = []
+let colourList = [];
+let activeColour;
+let activeHexomino = [];
 let myGravity;
 let hitEdge = false;
 let nameOfActiveHexo;
 
 function setup() {
   createCanvas(600, 300);
-  makeGrid()
-  gravityOn()
-   colourList.push("#DA2C38")
-  colourList.push("#226F54")
-  colourList.push("yellow")
-  colourList.push("blue")
-  
+  makeGrid();
+  gravityOn();
+  colourList.push("#DA2C38");
+  colourList.push("#226F54");
+  colourList.push("yellow");
+  colourList.push("blue");
+
   newHexomino();
 }
 function draw() {
   background(220);
-  for(let i=0 ; i<grid.length; i++){
-    grid[i].display() 
+  for (let i = 0; i < grid.length; i++) {
+    grid[i].display();
   }
-  if(hitEdge){
+  if (hitEdge) {
     clearInterval(myGravity);
   }
-  if(isColumnClear(19)){
-    clearColumn(19)
+  for (let i = 0; i < 20; i++) {
+    if (isColumnClear(i)) {
+      clearColumn(i);
+    }
   }
 }
-function newHexomino(){
-  
-  let index = Math.trunc(random(0,colourList.length))
-  activeColour = colourList[index]
-  if(index == 0){
+function newHexomino() {
+  let index = Math.trunc(random(0, colourList.length));
+  activeColour = colourList[index];
+  if (index == 0) {
     makeCross();
-    nameOfActiveHexo = "cross"
-  } else if(index == 1){
-    makeTophat()
-    nameOfActiveHexo = "topHat"
-  } else if(index == 2){
-    makeRectangle()
-    nameOfActiveHexo = "rectangle"
-  }else if(index == 3){
-    makeStairCase()
-    nameOfActiveHexo = "stairCase"
-    
+    nameOfActiveHexo = "cross";
+  } else if (index == 1) {
+    makeTophat();
+    nameOfActiveHexo = "topHat";
+  } else if (index == 2) {
+    makeRectangle();
+    nameOfActiveHexo = "rectangle";
+  } else if (index == 3) {
+    makeStairCase();
+    nameOfActiveHexo = "stairCase";
   }
 }
-function gravityOn(){
- 
-  myGravity = setInterval(moveActiveHexomino,500)
+function gravityOn() {
+  myGravity = setInterval(moveActiveHexomino, 500);
 }
