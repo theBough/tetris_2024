d1917c89d27802d9d06d90a9179d7dd2fd49452f
corbin Boughen <50217007+theBough@users.noreply.github.com>
2025-01-07 13:14:34 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,26 +9,28 @@ function setup() {
   createCanvas(600, 300);
   makeGrid()
   gravityOn()
-  colourList.push("#DA2C38")
+   colourList.push("#DA2C38")
   colourList.push("#226F54")
   colourList.push("yellow")
   colourList.push("blue")
   
- newHexomino()
+  newHexomino();
 }
 function draw() {
   background(220);
   for(let i=0 ; i<grid.length; i++){
-    grid[i].display()
-    
+    grid[i].display() 
   }
- 
   if(hitEdge){
     clearInterval(myGravity);
   }
+  if(isColumnClear(19)){
+    clearColumn(19)
+  }
 }
 function newHexomino(){
-   let index = Math.trunc(random(0,colourList.length))
+  
+  let index = Math.trunc(random(0,colourList.length))
   activeColour = colourList[index]
   if(index == 0){
     makeCross();
