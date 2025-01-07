47074d6d5397f24517c4887ad1c646c35d2762e0
corbin Boughen <50217007+theBough@users.noreply.github.com>
2025-01-07 13:30:23 -0600

    Update moveHexomino.js

--- moveHexomino.js
+++ moveHexomino.js
@@ -1,98 +1,123 @@
 let newHexominoTime = false;
-let finishedHexomino = []
-function moveActiveHexomino(){
-  
-  for(let i = 0; i<activeHexomino.length-1;i++){
-    activeHexomino[i] +=1
-    for(let j = 0; j<finishedHexomino.length; j++){
-      if(activeHexomino[i] +1== finishedHexomino[j]){
-        newHexominoTime = true
-      } 
-    }
-    
-    //check to see if it hits bottom
-    if(activeHexomino[i] % 20 == 19){
-      //hitEdge = true
-      newHexominoTime = true
-    }
-    grid[activeHexomino[i]-1].col = 100
-
-  }
-    for(let i = 0; i<activeHexomino.length-1;i++){
-      grid[activeHexomino[i]].col = activeColour
-    }    
-    if(newHexominoTime){
-      for(let i = 0; i<activeHexomino.length-1;i++){
-        finishedHexomino.push(activeHexomino[i])
+let finishedSquares = [];
+function moveActiveHexomino() {
+  for (let i = 0; i < activeHexomino.length - 1; i++) {
+    activeHexomino[i] += 1;
+    for (let j = 0; j < finishedSquares.length; j++) {
+      if (activeHexomino[i] + 1 == finishedSquares[j]) {
+        newHexominoTime = true;
       }
-      newHexomino();
-      newHexominoTime = false
-      //console.log(finishedHexomino)
     }
+    if (activeHexomino[i] % 20 == 19) {
+      //hitEdge = true
+      newHexominoTime = true;
+    }
+    grid[activeHexomino[i] - 1].col = 100;
+  }
+
+  for (let i = 0; i < activeHexomino.length - 1; i++) {
+    grid[activeHexomino[i]].col = activeColour;
+  }
+
+  if (newHexominoTime) {
+    //add the hexomino that just touched to the array.
+    for (let i = 0; i < activeHexomino.length - 1; i++) {
+      finishedSquares.push(activeHexomino[i]);
+    }
+    newHexomino();
+    newHexominoTime = false;
+    console.log(finishedSquares);
+  }
 }
-function keyReleased(){
-  if(key === 'w'){
-    for(let i = 0; i<activeHexomino.length-1;i++){
-     if(activeHexomino[i] < 20){
-       //we are on the edge, and cant move up
-       return 0;
-     }
+function keyReleased() {
+  if (key === "w") {
+    for (let i = 0; i < activeHexomino.length - 1; i++) {
+      for (let j = 0; j < finishedSquares.length; j++) {
+        if (activeHexomino[i] - 20 == finishedSquares[j]) {
+          return 0;
+        }
+      }
+
+      if (activeHexomino[i] < 20) {
+        //we are on the edge, and cant move up
+        return 0;
+      }
     }
     //move the tetromino up
-   for(let i = 0; i<activeHexomino.length-1;i++){
-     activeHexomino[i] -= 20
+    for (let i = 0; i < activeHexomino.length - 1; i++) {
+      activeHexomino[i] -= 20;
     }
-    //true the old spot grey
-    for(let i = 0; i<activeHexomino.length-1;i++){
-      grid[activeHexomino[i]+20].col = 100
-    } 
+    //turn the old spot grey
+    for (let i = 0; i < activeHexomino.length - 1; i++) {
+      grid[activeHexomino[i] + 20].col = 100;
+    }
     //colour new location of tetromino
-    for(let i = 0; i<activeHexomino.length-1;i++){
-      grid[activeHexomino[i]].col = activeColour
-    } 
+    for (let i = 0; i < activeHexomino.length - 1; i++) {
+      grid[activeHexomino[i]].col = activeColour;
+    }
   }
-  if(key === 's'){
-    for(let i = 0; i<activeHexomino.length-1;i++){
-     if(activeHexomino[i] > 180){
-       //we are on the edge, and cant move down
-       return 0;
-     }
+  if (key === "s") {
+    for (let i = 0; i < activeHexomino.length - 1; i++) {
+      for (let j = 0; j < finishedSquares.length; j++) {
+        if (activeHexomino[i] + 20 == finishedSquares[j]) {
+          return 0;
+        }
+      }
+    }
+    for (let i = 0; i < activeHexomino.length - 1; i++) {
+      if (activeHexomino[i] > 180) {
+        //we are on the edge, and cant move down
+        return 0;
+      }
     }
     //move the tetromino down
-   for(let i = 0; i<activeHexomino.length-1;i++){
-     activeHexomino[i] += 20
+    for (let i = 0; i < activeHexomino.length - 1; i++) {
+      activeHexomino[i] += 20;
     }
     //true the old spot grey
-    for(let i = 0; i<activeHexomino.length-1;i++){
-      grid[activeHexomino[i]-20].col = 100
+    for (let i = 0; i < activeHexomino.length - 1; i++) {
+      grid[activeHexomino[i] - 20].col = 100;
     }
     //colour new location of tetromino
-    for(let i = 0; i<activeHexomino.length-1;i++){
-      grid[activeHexomino[i]].col = activeColour
+    for (let i = 0; i < activeHexomino.length - 1; i++) {
+      grid[activeHexomino[i]].col = activeColour;
     }
   }
-  if(key === 'a'){
+  if (key === "a") {
     whatHexomino();
   }
-   if(key === 'q'){
-      whatHexomino()
-   }
+  if (key === "q") {
+    whatHexomino();
+  }
+  if (key === "d") {
+    //move the tetromino up
+    for (let i = 0; i < activeHexomino.length - 1; i++) {
+      activeHexomino[i] += 1;
+    }
+    //turn the old spot grey
+    for (let i = 0; i < activeHexomino.length - 1; i++) {
+      grid[activeHexomino[i] - 1].col = 100;
+    }
+    //colour new location of tetromino
+    for (let i = 0; i < activeHexomino.length - 1; i++) {
+      grid[activeHexomino[i]].col = activeColour;
+    }
+  }
 }
 
-function whatHexomino(){
-  
-  switch(nameOfActiveHexo){
-      case "topHat":
-        topHatState();
+function whatHexomino() {
+  switch (nameOfActiveHexo) {
+    case "topHat":
+      topHatState();
       break;
-      case "cross":
-        crossState();
+    case "cross":
+      crossState();
       break;
-      case "rectangle":
-        rectangleState();
+    case "rectangle":
+      rectangleState();
       break;
-      case "stairCase":
-        stairCaseState();
+    case "stairCase":
+      stairCaseState();
       break;
-  }//end of switch
-}//end of function
+  } //end of switch
+} //end of function
